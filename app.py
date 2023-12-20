02370991dde609be4b5564edb2e26b7705ffef35
Tanisha Arora <tanisharora2002@gmail.com>
2023-12-20 23:23:40 +0530

    my commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask,request,render_template
+
+app=Flask(__name__)
+
+
+@app.route('/')
+def welcome():
+    return "welcome to flask tutorials"
+
+@app.route('/cal',methods=["GET"])
+def maths():
+    operation=request.json["operation"]
+    number1=request.json[number1]
+    number2=request.json[number2]
+    
+    if operation=="add":
+        result=number1+number2
+    elif operation=="multiply":
+        result=number1*number2
+    elif operation=="divide":
+        result=number1/number2   
+    else:
+        result=number1-number2
+    return result             
+    
+
+if __name__ == '__main__':
+    app.run(debug=True)    
